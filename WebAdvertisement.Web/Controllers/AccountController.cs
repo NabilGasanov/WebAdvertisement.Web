d229bd856f4630e4249053e0c8a1f759bb79047a
Nabil Hasanov <nabil2794@hotmail.com>
2020-08-29 13:38:01 +0400

    c

---
+++ WebAdvertisement.Web/Controllers/AccountController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvertisement.Web.Models.Accounts;
+
+namespace WebAdvertisement.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public AccountController(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager,
+            CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+        public async Task<IActionResult> SignUp()
+        {
+            var model = new SignUpViewModel();
+            return View(model);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = _pool.GetUser(model.Email);
+                    if (user.Status != null)
+                    {
+                        ModelState.AddModelError("UserExists", "User with this email already exists");
+                        return View(model);
+                    }
+
+                    var cognitoAttributeName = CognitoAttribute.Name.AttributeName;
+                    user.Attributes.Add(cognitoAttributeName, model.Email);
+                    var createdUser = await _userManager.CreateAsync(user, model.Password).ConfigureAwait(false);
+                    if (createdUser.Succeeded)
+                    {
+                       return RedirectToAction("Confirm");
+                    }
+                }
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        public async Task<IActionResult> Confirm()
+        {
+            var model = new ConfirmViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Confirm(ConfirmViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user is null)
+                {
+                    ModelState.AddModelError("NotFound", "A use with th given not found");
+                    return View(model);
+                }
+
+                var result = await ((CognitoUserManager<CognitoUser>)_userManager).ConfirmSignUpAsync(user, model.Code, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.Code, item.Description);
+                }
+
+                return View(model);
+            }
+
+            return View(model);
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("login")]
+        public async Task<IActionResult> LoginSignIn(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+                
+                ModelState.AddModelError("LoginError","Email or password do not match");
+            }
+
+            return View(model);
+        }
+    }
+}
